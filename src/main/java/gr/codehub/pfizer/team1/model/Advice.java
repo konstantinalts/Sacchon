30290e174afb4265638bb47cd59c2a0a0a508d94
Tbakaloudi <theodora.bakaloudi@gmail.com>
2021-04-10 23:10:09 +0300

    Advice Class

---
+++ src/main/java/gr/codehub/pfizer/team1/model/Advice.java
@@ -0,0 +1,30 @@
+package gr.codehub.pfizer.team1.model;
+
+import lombok.Data;
+
+import javax.persistence.*;
+import java.sql.Time;
+import java.util.Date;
+
+@Data
+@Entity
+public class Advice {
+
+    @Id
+    @Column(name = "id", nullable = false)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "date", nullable = false)
+    private Date date;
+
+    @Column(name = "medication", nullable = true)
+    private Medication medication;
+
+    @Column(name = "dosage", nullable = true)
+    private float dosage;
+
+    @Column(name = "description", nullable = true)
+    private String description;
+
+}
