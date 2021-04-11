b4502561307b4357a8fade03ae1646a6461998a8
Tbakaloudi <theodora.bakaloudi@gmail.com>
2021-04-11 13:50:43 +0300

    table joins

--- src/main/java/gr/codehub/pfizer/team1/model/DoctorAdvice.java
+++ src/main/java/gr/codehub/pfizer/team1/model/DoctorAdvice.java
@@ -9,7 +9,7 @@ import java.util.Date;
 
 @Data
 @Entity
-public class Advice {
+public class DoctorAdvice {
 
     @Id
     @Column(name = "id", nullable = false)
@@ -19,6 +19,7 @@ public class Advice {
     @Column(name = "date", nullable = false)
     private Date date;
 
+    @Enumerated(EnumType.STRING)
     @Column(name = "medication", nullable = true)
     private Medication medication;
 
@@ -27,5 +28,11 @@ public class Advice {
 
     @Column(name = "description", nullable = true)
     private String description;
+
+    @ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    private Doctor doctor;
+
+    @ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    private Patient patient;
 
 }
--- src/main/java/gr/codehub/pfizer/team1/model/Patient.java
+++ src/main/java/gr/codehub/pfizer/team1/model/Patient.java
@@ -5,6 +5,7 @@ import lombok.Data;
 import javax.persistence.*;
 import java.sql.Time;
 import java.util.Date;
+import java.util.List;
 
 @Data
 @Entity
@@ -32,5 +33,20 @@ public class Patient {
 
     @Column(name = "email", nullable = false)
     private String email;
+
+    @Column(name = "username", nullable = false)
+    private String username;
+
+    @Column(name = "password", nullable = false)
+    private String password;
+
+    @ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    private Doctor doctor;
+
+    @OneToMany(mappedBy = "mediDataRepo", fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    private List<MediDataRepo> mediDataRepos;
+
+    @OneToMany(mappedBy = "doctorAdvice", fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    private List<DoctorAdvice> doctorAdvices;
 
 }
