08422091f0358a16d55de5887081fa1db40b79b7
MariosGen <mariosgen@athtech.gr>
2021-04-13 21:11:20 +0300

    CorsFilter added in Main

--- src/main/java/gr/codehub/pfizer/team1/MainApp.java
+++ src/main/java/gr/codehub/pfizer/team1/MainApp.java
@@ -5,11 +5,15 @@ import gr.codehub.pfizer.team1.router.CustomRouter;
 import org.restlet.Application;
 import org.restlet.Component;
 import org.restlet.Restlet;
+import org.restlet.data.Method;
 import org.restlet.data.Protocol;
 import org.restlet.engine.Engine;
+import org.restlet.engine.application.CorsFilter;
 import org.restlet.routing.Router;
 
 import javax.persistence.EntityManager;
+import java.util.Arrays;
+import java.util.HashSet;
 import java.util.logging.Logger;
 
 public class MainApp extends Application {
@@ -35,6 +39,17 @@ public class MainApp extends Application {
     public Restlet createInboundRoot() {
         CustomRouter customRouter = new CustomRouter(this);
         Router publicRouter = customRouter.publicResources();
+
+
+        CorsFilter corsFilter = new CorsFilter(getContext(),publicRouter);
+        corsFilter.setAllowedCredentials(true);
+        corsFilter.setAllowedOrigins(new HashSet<>(Arrays.asList("*")));
+        HashSet<Method> methodHashSet = new HashSet<>();
+        methodHashSet.add(Method.GET);
+        methodHashSet.add(Method.POST);
+        methodHashSet.add(Method.PUT);
+        methodHashSet.add(Method.DELETE);
+        corsFilter.setDefaultAllowedMethods(methodHashSet);
 
         return publicRouter;
     }
